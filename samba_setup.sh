ec15a95665b35c7f26b25800a337859649b5de3f
aclegg2011 <44304193+aclegg2011@users.noreply.github.com>
2018-11-18 18:41:22 -0800

    Update samba_setup.sh

--- samba_setup.sh
+++ samba_setup.sh
@@ -5,8 +5,10 @@ sudo smbpasswd -L -e username
 sudo smbpasswd -L -a username
 sudo mkir /media/samba/NAS
 sudo chmod 0777 /media/samba/NAS
-sudo chown -R nas /media/samba/NAS
+sudo chown -R main_account:nas /media/samba
+sudo usermod -a -G nas main_account
 edit fstab and add NAS drive to mount /media/samba/NAS
+
 sudo nano /etc/samba/smb.conf
 [Movies]
 ;
@@ -20,5 +22,20 @@ directory mask = 0755
 force user = nas
 ;
 force group = nas
+
+[NAS]
+;
+path = /media/samba/NAS
+browseable = yes
+writable = yes
+guest ok = no
+create mask = 0644
+directory mask = 0755
+;
+force user = main_account
+;
+force group = nas
+
+
 save and edit
 sudo /etc/init.d/smbd restart
